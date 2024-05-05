c26ec56470f953e26c49a169eb9648ae395e4b6e
silleruss <silleruss@gmail.com>
2024-05-05 18:00:30 +0900

    chore: move package of HealthController

--- gotanda/src/main/kotlin/com/silleruss/gotanda/controllers/health/HealthController.kt
+++ gotanda/src/main/kotlin/com/silleruss/gotanda/controllers/health/HealthController.kt
@@ -1,4 +1,4 @@
-package com.silleruss.gotanda.controllers
+package com.silleruss.gotanda.controllers.health
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
