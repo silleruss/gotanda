87514c35a4e33cedb17dd56c7ccb813a04d3928b
silleruss <silleruss@gmail.com>
2024-05-06 23:00:05 +0900

    chore: add toFlux, kstNow methods

--- gotanda/src/main/kotlin/com/silleruss/gotanda/core/Extensions.kt
+++ gotanda/src/main/kotlin/com/silleruss/gotanda/core/Extensions.kt
@@ -1,7 +1,10 @@
 package com.silleruss.gotanda.core
 
 import arrow.core.Either
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import java.time.LocalDateTime
+import java.time.ZoneId
 import java.util.*
 
 inline fun <L, R: Any> Optional<R>.toEither(item: () -> L): Either<L, R> = if (isPresent) Either.Right(get()) else Either.Left(item())
@@ -9,3 +12,7 @@ inline fun <L, R: Any> Optional<R>.toEither(item: () -> L): Either<L, R> = if (i
 fun <T: Any> T?.toOptional(): Optional<T> = this?.let { Optional.of(it) } ?: Optional.empty()
 
 fun <T : Any> Either<Throwable, T>.toMono() = fold({ error: Throwable -> Mono.error(error) }) { data: T -> Mono.just(data) }
+
+fun <T: Any> Either<Throwable, List<T>>.toFlux() = fold({ error: Throwable -> Flux.error(error) }) { list: List<T> -> Flux.fromIterable(list) }
+
+fun kstNow(): LocalDateTime = LocalDateTime.now(ZoneId.of("Asia/Seoul"))
